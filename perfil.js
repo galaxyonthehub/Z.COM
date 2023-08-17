77ce7cde2acbcaf6fa474f40ff7af19078bec7b7
galaxyonthehub <117865433+galaxyonthehub@users.noreply.github.com>
2023-08-17 15:44:01 -0300

    Add files via upload

---
+++ perfil.js
@@ -0,0 +1,9 @@
+function main() {
+      window.location = "Feed.html";
+  }
+  function profile() {
+    window.location = "meuperfil.html";
+}
+function photo() {
+    window.location = "photo.html";
+}
